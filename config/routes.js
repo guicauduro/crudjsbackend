f056f4aa9de455324f9ed45d1e62bd9f96644519
Guilherme Cauduro <guilhermekof@hotmail.com>
2019-08-08 16:43:34 -0300

    liberacao administrador

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,5 @@
+const admin = require('/admin')
+
 module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
@@ -5,18 +7,18 @@ module.exports = app => {
 
     app.route('/users')
         .all(app.config.passport.authenticate()) // todos os metodos passam por esse filtro que pode impedir de ir ao metodo abaixo
-        .post(app.api.user.save) //inserir
-        .get(app.api.user.get) // pegar
+        .post(admin(app.api.user.save)) //inserir
+        .get(admin(app.api.user.get)) // pegar
 
     app.route('/users/:id')
         .all(app.config.passport.authenticate())
-        .put(app.api.user.save) // alterar
-        .get(app.api.user.getById)
+        .put(admin(app.api.user.save)) // alterar
+        .get(admin(app.api.user.getById))
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
-        .get(app.api.category.get)
-        .post(app.api.category.save)
+        .get(admin(app.api.category.get))
+        .post(admin(app.api.category.save))
 
     // Cuidar com a ordem, os mais especificos devem vir antes dos mais genéricos
     app.route('/categories/tree')
@@ -26,19 +28,19 @@ module.exports = app => {
     app.route('/categories/:id')
         .all(app.config.passport.authenticate())
         .get(app.api.category.getById)
-        .put(app.api.category.save)
-        .delete(app.api.category.remove)
+        .put(admin(app.api.category.save))
+        .delete(admin(app.api.category.remove))
 
     app.route('/articles')
         .all(app.config.passport.authenticate())
-        .get(app.api.article.get)
-        .post(app.api.article.save)
+        .get(admin(app.api.article.get))
+        .post(admin(app.api.article.save))
 
     app.route('/articles/:id')
         .all(app.config.passport.authenticate())
         .get(app.api.article.getById)
-        .put(app.api.article.save)
-        .delete(app.api.article.remove)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
 
     app.route('/categories/:id/articles')
         .all(app.config.passport.authenticate())
